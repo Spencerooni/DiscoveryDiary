75f1801f9865d0a2b8067a1bd9d8cf175d65a6d4
Eamon Compston <eamonc@inferno.local>
2016-11-04 14:29:48 +0000

    removed unused import

--- src/main/java/com/kainos/discoverydiary/resources/MediaResource.java
+++ src/main/java/com/kainos/discoverydiary/resources/MediaResource.java
@@ -12,7 +12,6 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
