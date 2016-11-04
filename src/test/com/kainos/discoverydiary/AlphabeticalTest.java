15f1c932143ffc75ba5ff04935bb27958a711b74
Aoife <A.Gildernew@kainos.com>
2016-11-04 14:49:25 +0000

    Fixing original test to fix and added test to show it passes when it fails

--- src/test/com/kainos/discoverydiary/AlphabeticalTest.java
+++ src/test/com/kainos/discoverydiary/AlphabeticalTest.java
@@ -30,9 +30,34 @@ public class AlphabeticalTest {
         //Act
         Homepage result = mediaResource.Homepage();
 
+        System.out.println(result.getMedias().get(0));
         //Assert
-        result.getMedias().equals("Advances in Relationship Marketing");
-        result.getMedias().equals("Oat So Simple");
-        result.getMedias().equals("Windows Server 2003 Network Infrastructure");
+        Assert.assertEquals((result.getMedias().get(0).getTitle()), "Advances in Relationship Marketing");
+        Assert.assertEquals((result.getMedias().get(1).getTitle()), "Oat So Simple");
+        Assert.assertEquals((result.getMedias().get(2).getTitle()), "Windows Server 2003 Network Infrastructure");
+    }
+
+    @Test
+    public void getAllMediaReturnsMediaInAlphabeticalOrderShowFails() {
+        //Arrange
+        MediaResource mediaResource = new MediaResource();
+
+        Media media = new Media("Advances in Relationship Marketing", "Adrian Payne", "Advances in Relationship Marketing by Adrian Payne", Category.TECHNICAL, "1995", "https://www.asme.org/getmedia/c2c8ea5a-b690-4ba7-92bb-34bd1432862b/book_guide_hero_books.aspx");
+        DataStore.medias.put(media.getId(), media);
+
+        Media media2 = new Media("Windows Server 2003 Network Infrastructure", "Craig Zacker", "Study guide for MCSE Exam 70-293", Category.TECHNICAL, "2004", "https://images-na.ssl-images-amazon.com/images/I/51Zu4ldN6PL._SX408_BO1,204,203,200_.jpg");
+        DataStore.medias.put(media2.getId(), media2);
+
+        Media media3 = new Media("Oat So Simple", "Quaker", "9 sachets of porridge", Category.NON_TECHNICAL, "2016", "https://www.quaker.co.uk/images/default-source/products/quaker-traditional-oats/quaker-oats-1kg.png?sfvrsn=8");
+        DataStore.medias.put(media3.getId(), media3);
+
+        //Act
+        Homepage result = mediaResource.Homepage();
+
+        System.out.println(result.getMedias().get(0));
+        //Assert
+        Assert.assertNotEquals((result.getMedias().get(2).getTitle()), "Advances in Relationship Marketing");
+        Assert.assertNotEquals((result.getMedias().get(0).getTitle()), "Oat So Simple");
+        Assert.assertNotEquals((result.getMedias().get(1).getTitle()), "Windows Server 2003 Network Infrastructure");
     }
 }
