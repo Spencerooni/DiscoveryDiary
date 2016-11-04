ae4a5ca7f081546397a0cb0c2c9381307ee5b8e3
Eamon Compston <eamonc@inferno.local>
2016-11-04 13:04:18 +0000

    Deleted empty txt file and fixed MediaStatusTests

--- src/test/com/kainos/discoverydiary/MediaStatusTest.java
+++ src/test/com/kainos/discoverydiary/MediaStatusTest.java
@@ -14,7 +14,7 @@ public class MediaStatusTest {
     @Test
     public void ifMediaAvailableShowStatus() {
         //Arrange
-        Media media = new Media("Java For Dummies", "J K Bowling", "Training book on java", Category.TECHNICAL, "2001");
+        Media media = new Media("Java For Dummies", "J K Bowling", "Training book on java", Category.TECHNICAL, "2001", "https://www.asme.org/getmedia/c2c8ea5a-b690-4ba7-92bb-34bd1432862b/book_guide_hero_books.aspx");
         DataStore.medias.put(media.getId(), media);
         MediaResource mediaResource = new MediaResource();
 
@@ -28,7 +28,7 @@ public class MediaStatusTest {
     @Test
     public void ifMediaInDisrepairShowStatus() {
         //Arrange
-        Media media = new Media("The Big Book of Java", "Stephen Hawking", "Training book on Java", Category.TECHNICAL, "1980", null, Status.IN_DISREPAIR);
+        Media media = new Media("The Big Book of Java", "Stephen Hawking", "Training book on Java", Category.TECHNICAL, "1980", null, Status.IN_DISREPAIR, "https://www.asme.org/getmedia/c2c8ea5a-b690-4ba7-92bb-34bd1432862b/book_guide_hero_books.aspx");
         DataStore.medias.put(media.getId(), media);
         MediaResource mediaResource = new MediaResource();
 
@@ -42,7 +42,7 @@ public class MediaStatusTest {
     @Test
     public void ifMediaOnLoanShowStatusAndLoaneeDetails() {
         //Arrange
-        Media media = new Media("The ins and outs of Ruby on Rails", "Mail Serrif", "Training book on Ruby", Category.TECHNICAL, "1980", "Sean", Status.ON_LOAN);
+        Media media = new Media("The ins and outs of Ruby on Rails", "Mail Serrif", "Training book on Ruby", Category.TECHNICAL, "1980", "Sean", Status.ON_LOAN, "https://www.asme.org/getmedia/c2c8ea5a-b690-4ba7-92bb-34bd1432862b/book_guide_hero_books.aspx");
         DataStore.medias.put(media.getId(), media);
         MediaResource mediaResource = new MediaResource();
 
